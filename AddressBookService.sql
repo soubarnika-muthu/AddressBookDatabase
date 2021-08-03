296b75c14ba8bd8e1ccb21aac0e0343e2837bb75
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-08-03 21:12:07 +0530

    [Soubarnika]Creating address book Table

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,2 +1,16 @@
 --UC1 Creating the database AddressBookService
 create database AddressBookService
+
+--UC2 Creating address book table with first name, last name,address,city,state,phone number,Zipcode,email address
+create table AddressBookTable
+(
+  personId int identity(1,1) primary key,
+  firstName varchar(100),
+  lastName varchar(100),
+  address varchar(200),
+  city varchar(50),
+  state varchar(100),
+  zipCode bigint,
+  phoneNumber bigint,
+  email varchar(50)
+)
